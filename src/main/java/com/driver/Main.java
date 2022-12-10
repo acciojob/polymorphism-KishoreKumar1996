04bdacd37fa11dd6b17c39cd3f99bb0268f1195c
KishoreKumar1996 <kishorem11259@gmail.com>
2022-12-10 20:22:39 +0530

    poly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static class product {
+        public int product(int x, int y) {
+            return x;
+        }
+        public int product(int x, int y, int z) {
+            return x;
+        }
+        public double product(double x, double y) {
+            return x;
+        }
+    }
 
+    public static void main(String[] args) {
+        product p=new product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.3,2.8);
+    }
 }
