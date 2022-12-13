e8284120e8694282a02714664c19c05cc0dafaec
KishoreKumar1996 <kishorem11259@gmail.com>
2022-12-13 23:22:29 +0530

    pushing

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,25 +2,25 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        product p=new product();
-        int ans1=p.product(10,25);
-        int ans2=p.product(13,22,31);
-        double ans3= p.product(1.3,5.6);
+        Product p=new Product();
+        int ans1=p.Product(10,25);
+        int ans2=p.Product(13,22,31);
+        double ans3= p.Product(1.3,5.6);
         System.out.println(ans1+" "+ans2+" "+ans3);
     }
 
-        public static class product {
-           public int product(int x, int y) {
+        public static class Product {
+           public int Product(int x, int y) {
 
                 return x * y;
             }
 
-            public int product(int x, int y, int z) {
+            public int Product(int x, int y, int z) {
 
                 return x * y * z;
             }
 
-            public double  product(double x, double y) {
+            public double  Product(double x, double y) {
 
                 return x * y;
             }
