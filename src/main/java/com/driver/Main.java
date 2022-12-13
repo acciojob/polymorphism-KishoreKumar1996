4251c39988694487fc167cdd01957effb91478d4
KishoreKumar1996 <kishorem11259@gmail.com>
2022-12-13 23:25:22 +0530

    copp

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,24 +3,24 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         Product p=new Product();
-        int ans1=p.Product(10,25);
-        int ans2=p.Product(13,22,31);
-        double ans3= p.Product(1.3,5.6);
+        int ans1=p.product(10,25);
+        int ans2=p.product(13,22,31);
+        double ans3= p.product(1.3,5.6);
         System.out.println(ans1+" "+ans2+" "+ans3);
     }
 
         public static class Product {
-           public int Product(int x, int y) {
+           public int product(int x, int y) {
 
                 return x * y;
             }
 
-            public int Product(int x, int y, int z) {
+            public int product(int x, int y, int z) {
 
                 return x * y * z;
             }
 
-            public double  Product(double x, double y) {
+            public double  product(double x, double y) {
 
                 return x * y;
             }
